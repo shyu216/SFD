4417ac4aee9ef752cc434dd4e647dc9ff7f0d0ad
shyu216 <79613808+shyu216@users.noreply.github.com>
2023-01-31 15:46:32 +0800

    update

--- depth_to_lidar.py
+++ depth_to_lidar.py
@@ -30,7 +30,7 @@ def depth_to_lidar(idx_filename, split, dense_depth_dir, pseudo_depth_dir):
     for data_idx in tqdm.tqdm(data_idx_list):
         calib = dataset.get_calibration(data_idx)
         img_rgb = dataset.get_image(data_idx)
-        dense_img_filename = os.path.join(dense_depth_dir, '%010d.png'%(data_idx))
+        dense_img_filename = os.path.join(dense_depth_dir, '%06d.png'%(data_idx))
         img_depth_small = depth_read(dense_img_filename)
         h, w, _ = img_rgb.shape
         th,tw = img_depth_small.shape
@@ -60,21 +60,20 @@ print('---------------Start to generate training pseudo point clouds------------
 depth_to_lidar( \
     '../OpenPCDet/data/kitti/ImageSets/train.txt',
     'training',
-    '../testresult',
-    # 'data/kitti_sfd_seguv_twise/training/depth_pseudo_rgbseguv_twise'
-    '../new'
+    '../training',
+    'data/kitti_sfd_seguv_twise/training/depth_pseudo_rgbseguv_twise'
 )
-# depth_to_lidar( \
-#     'data/kitti_sfd_seguv_twise/ImageSets/val.txt',
-#     'training',
-#     '../testresult',
-#     'data/kitti_sfd_seguv_twise/training/depth_pseudo_rgbseguv_twise'
-# )
+depth_to_lidar( \
+    '../OpenPCDet/data/kitti/ImageSets/val.txt',
+    'training',
+    '../training',
+    'data/kitti_sfd_seguv_twise/training/depth_pseudo_rgbseguv_twise'
+)
 
-# print('---------------Start to generate testing pseudo point clouds---------------')
-# depth_to_lidar( \
-#     'data/kitti_sfd_seguv_twise/ImageSets/test.txt',
-#     'testing',
-#     'data/kitti_sfd_seguv_twise/testing/depth_dense_twise',
-#     'data/kitti_sfd_seguv_twise/testing/depth_pseudo_rgbseguv_twise'
-# )
+print('---------------Start to generate testing pseudo point clouds---------------')
+depth_to_lidar( \
+    '../OpenPCDet/data/kitti/ImageSets/test.txt',
+    'testing',
+    '../testing/',
+    'data/kitti_sfd_seguv_twise/testing/depth_pseudo_rgbseguv_twise'
+)
